2e72604bb90f7fb181079c7c74f585856e1067ba
Marius Fylling <marius@sharptickets.net>
2018-03-20 09:59:25 +0100

    Python3?

--- bot.py
+++ bot.py
@@ -140,7 +140,7 @@ def start_bot():
 
 
 def stop_bot(self):
-    print "Trying to stop thread "
+    print()"Trying to stop thread")
     if self.process is not None:
         self.process.terminate()
         self.process = None
